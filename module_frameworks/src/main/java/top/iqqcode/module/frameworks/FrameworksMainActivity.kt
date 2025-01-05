2259427473ce6b38cc7a514140ba0933423fd661
iqqcode <iqqcode@qq.com>
2025-01-05 12:45:17 +0800

    [Story] 0105-新建module_frameworks模块，进阶Handler知识

---
+++ module_frameworks/src/main/java/top/iqqcode/module/frameworks/FrameworksMainActivity.kt
@@ -0,0 +1,20 @@
+package top.iqqcode.module.frameworks
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class FrameworksMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_frameworks)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
